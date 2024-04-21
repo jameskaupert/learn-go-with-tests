a573607dd98f71b6389ef1cf014b1bc9cb9e8bed
James Kaupert <jameskaupert@gmail.com>
2024-04-21 16:43:28 +0000

    update hello world with refactor

--- hello.go
+++ hello.go
@@ -2,8 +2,13 @@ package hello
 
 import "fmt"
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
-	return fmt.Sprintf("Hello, %s", name)
+	if name == "" {
+		name = "World"
+	}
+	return englishHelloPrefix + name
 }
 
 func main() {
