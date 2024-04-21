2088a4400d5138cb742d31ccf94070a2e7434c1c
James Kaupert <jameskaupert@gmail.com>
2024-04-21 16:36:09 +0000

    start with hello world example

---
+++ hello_test.go
@@ -0,0 +1,12 @@
+package hello
+
+import "testing"
+
+func TestHello(t *testing.T) {
+	got := Hello("James")
+	want := "Hello, James"
+
+	if got != want {
+		t.Errorf("got %q want %q", got, want)
+	}
+}
