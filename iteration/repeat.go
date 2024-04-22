5d20a30c9318059c8537dc13276a40530ba3efd2
James Kaupert <jameskaupert@gmail.com>
2024-04-22 03:49:14 +0000

    basic iteration example

---
+++ iteration/repeat.go
@@ -0,0 +1,10 @@
+package iteration
+
+// Takes a character and repeats it repeatCount times, returning the resulting string
+func Repeat(character string, repeatCount int) string {
+	var repeated string
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	}
+	return repeated
+}
---
+++ iteration/repeat_test.go
@@ -0,0 +1,27 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 4)
+	expected := "aaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q, got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a", 5)
+	}
+}
+
+func ExampleRepeat() {
+	s := Repeat("a", 4)
+	fmt.Println(s)
+	// Output: aaaa
+}
