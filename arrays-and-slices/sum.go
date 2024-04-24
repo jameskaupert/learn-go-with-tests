c02a669256674ca3ebae9c9a357e343bc25fe1ea
James Kaupert <jameskaupert@gmail.com>
2024-04-24 02:42:56 +0000

    finish arrays and slices section

--- arrays-and-slices/sum.go
+++ arrays-and-slices/sum.go
@@ -7,3 +7,16 @@ func Sum(nums []int) int {
 	}
 	return total
 }
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, s := range numbersToSum {
+		if len(s) == 0 {
+			sums = append(sums, 0)
+		} else {
+			tail := s[1:]
+			sums = append(sums, Sum(tail))
+		}
+	}
+	return sums
+}
